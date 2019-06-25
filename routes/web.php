37a2ad5cf13846751787fd94d3c45bfd8f234676
vagrant <vagrant@homestead>
2019-06-25 07:02:25 +0000

    公共样式文件

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+session()->flash('success', 'This is a success alert—check it out!');
+session()->flash('danger', 'This is a danger alert—check it out!');
+session()->flash('warning', 'This is a warning alert—check it out!');
+session()->flash('info', 'This is a info alert—check it out!');
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::get('/', 'PagesController@root')->name('root');
