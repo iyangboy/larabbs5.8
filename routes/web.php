91c4e00f08d09c302c374bc37147ad3c389ddc9b
杨万里 <jerry.yang@transn.com>
2019-06-25 15:52:16 +0800

    基础页面结构

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+/*
 session()->flash('success', 'This is a success alert—check it out!');
 session()->flash('danger', 'This is a danger alert—check it out!');
 session()->flash('warning', 'This is a warning alert—check it out!');
 session()->flash('info', 'This is a info alert—check it out!');
+*/
 
 /*
 Route::get('/', function () {
