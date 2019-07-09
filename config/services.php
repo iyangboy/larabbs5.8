2dec40eadecf51a9546353fa426b442dbf9f6dfd
杨万里 <jerry.yang@transn.com>
2019-07-09 13:28:40 +0800

    6.8翻译 URL 标示

--- config/services.php
+++ config/services.php
@@ -44,4 +44,10 @@ return [
         ],
     ],
 
+    // 百度翻译
+    'baidu_translate' => [
+        'appid' => env('BAIDU_TRANSLATE_APPID', ''),
+        'key'   => env('BAIDU_TRANSLATE_KEY', ''),
+    ],
+
 ];
