8d53066101ba1efe861e9cdb072dc4cfe99e0a5e
杨万里 <jerry.yang@transn.com>
2019-07-09 10:12:37 +0800

    6.1用户可以创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -5,7 +5,15 @@ function route_class()
     return str_replace('.', '-', Route::currentRouteName());
 }
 
+// 标题选中
 function category_nav_active($category_id)
 {
     return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
+
+// 获取文章摘要
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -11,11 +11,21 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <!-- Left Side Of Navbar -->
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item {{ active_class(if_route('topics.index')) }}"><a class="nav-link" href="{{ route('topics.index') }}">话题</a></li>
-        <li class="nav-item {{ category_nav_active(1) }}"><a class="nav-link" href="{{ route('categories.show', 1) }}">分享</a></li>
-        <li class="nav-item {{ category_nav_active(2) }}"><a class="nav-link" href="{{ route('categories.show', 2) }}">教程</a></li>
-        <li class="nav-item {{ category_nav_active(3) }}"><a class="nav-link" href="{{ route('categories.show', 3) }}">问答</a></li>
-        <li class="nav-item {{ category_nav_active(4) }}"><a class="nav-link" href="{{ route('categories.show', 4) }}">公告</a></li>
+        <li class="nav-item {{ active_class(if_route('topics.index')) }}">
+          <a class="nav-link" href="{{ route('topics.index') }}">话题</a>
+        </li>
+        <li class="nav-item {{ category_nav_active(1) }}">
+          <a class="nav-link" href="{{ route('categories.show', 1) }}">分享</a>
+        </li>
+        <li class="nav-item {{ category_nav_active(2) }}">
+          <a class="nav-link" href="{{ route('categories.show', 2) }}">教程</a>
+        </li>
+        <li class="nav-item {{ category_nav_active(3) }}">
+          <a class="nav-link" href="{{ route('categories.show', 3) }}">问答</a>
+        </li>
+        <li class="nav-item {{ category_nav_active(4) }}">
+          <a class="nav-link" href="{{ route('categories.show', 4) }}">公告</a>
+        </li>
       </ul>
 
       <!-- Right Side Of Navbar -->
@@ -25,6 +35,11 @@
           <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
           <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
         @else
+          <li class="nav-item">
+            <a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
+              <i class="fa fa-plus"></i>
+            </a>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <img src="{{ Auth::user()->avatar ?? 'https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60' }}" class="img-responsive img-circle" width="30px" height="30px">
