622387b982a1fd3f1b8a6ac8fba88fdd0267a510
杨万里 <jerry.yang@transn.com>
2019-07-09 11:07:52 +0800

    6.5修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -21,6 +21,10 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+    	// XSS 数据过滤
+    	$topic->body = clean($topic->body, 'user_topic_body');
+
+    	// 获取摘要
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
