9033c0e58ea4dd854d409e3cd02fbb59f5fe4840
杨万里 <jerry.yang@transn.com>
2019-07-11 16:02:24 +0800

    7.7删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -58,4 +58,11 @@ class Topic extends Model
     {
         return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
+
+    // 获取评论数
+    public function updateReplyCount()
+    {
+        $this->reply_count = $this->replies->count();
+        $this->save();
+    }
 }
