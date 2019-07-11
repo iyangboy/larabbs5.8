37d398a4af308de423d61ff9451ba4ba39fbf5b1
杨万里 <jerry.yang@transn.com>
2019-07-11 15:19:46 +0800

    7.5消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,4 +76,12 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Reply::class);
     }
+
+    // 清除通知记录
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
 }
