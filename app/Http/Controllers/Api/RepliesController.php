87df32919fb0f06d426e6167b0099083b078c4db
杨万里 <jerry.yang@transn.com>
2019-07-25 17:27:53 +0800

    7.3回复列表replies index

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -4,14 +4,49 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Topic;
 use App\Models\Reply;
+use App\Models\User;
 use App\Transformers\ReplyTransformer;
 use App\Http\Requests\Api\ReplyRequest;
+use Illuminate\Http\Request;
 
 class RepliesController extends Controller
 {
-    public function index(Topic $topic)
+    public function index(Request $request, Topic $topic)
     {
+        // 关闭Dingo预加载
+        app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+
         $replies = $topic->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load(explode(',', $request->include));
+        }
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+
+
+        $replies = $topic->replies()->paginate(20);
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+
+    public function userIndex(Request $request, User $user)
+    {
+        /*
+        // 关闭Dingo预加载
+        app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+
+        $replies = $user->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load(explode(',', $request->include));
+        }
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+        */
+
+
+        $replies = $user->replies()->paginate(20);
 
         return $this->response->paginator($replies, new ReplyTransformer());
     }
--- routes/api.php
+++ routes/api.php
@@ -87,6 +87,9 @@ $api->version('v1', [
         // 话题回复列表
         $api->get('topics/{topic}/replies', 'RepliesController@index')
             ->name('api.topics.replies.index');
+        // 某个用户的回复列表
+        $api->get('users/{user}/replies', 'RepliesController@userIndex')
+            ->name('api.users.replies.index');
 
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
