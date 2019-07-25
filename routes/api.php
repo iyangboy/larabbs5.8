0efe82f5a15697b647f9d40dc4ddb429f43731bd
杨万里 <jerry.yang@transn.com>
2019-07-25 14:22:22 +0800

    6.3修改话题topic update api

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ $api->version('v2', function($api) {
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => 'serializer:array'
+    'middleware' => ['serializer:array', 'bindings']
 ], function($api) {
     $api->group([
         'middleware' => 'api.throttle',
@@ -92,6 +92,8 @@ $api->version('v1', [
             // 发布话题
             $api->post('topics', 'TopicsController@store')
                 ->name('api.topics.store');
+            $api->patch('topics/{topic}', 'TopicsController@update')
+                ->name('api.topics.update');
 
         });
 
