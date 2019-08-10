c267219a293f13502d932a42860bbbda031150b6
杨万里 <jerry.yang@transn.com>
2019-08-10 12:17:02 +0800

    4.2weapp login

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,7 @@ class User extends Authenticatable implements MustVerifyEmailContract, JWTSubjec
     protected $fillable = [
         'name', 'phone', 'email', 'password', 'introduction', 'avatar',
         'weixin_openid', 'weixin_unionid', 'registration_id',
+        'weixin_session_key', 'weapp_openid',
     ];
 
     /**
